72390411385f4905bdae4045d61cbc6490573fde
paulmoody <moodyweb@hotmail.com>
2014-10-21 17:10:20 -0430

    nuevas clases
    actualizacion de estilos

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
 var app = {
 
+	 showAlert: function (message, title) {
+	    if (navigator.notification) {
+	        navigator.notification.alert(message, null, title, 'OK');
+	    } else {
+	        alert(title ? (title + ": " + message) : message);
+	    }
+	},
+
     findByName: function() {
         console.log('findByName');
         this.store.findByName($('.search-key').val(), function(employees) {
             var l = employees.length;
             var e;
-            $('.employee-list').empty();
+           $('.employee-list').empty();
             for (var i=0; i<l; i++) {
                 e = employees[i];
                 $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
@@ -13,10 +21,30 @@ var app = {
         });
     },
 
-    initialize: function() {
-        this.store = new MemoryStore();
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+
+	renderHomeView: function() {
+    var html =
+            "<div class='header'><h1>Home</h1></div>" +
+            "<div class='search-view'>" +
+            "<input class='search-key'/>" +
+            "<ul class='employee-list'></ul>" +
+	            "</div>"
+	    $('body').html(html);
+	    $('.search-key').on('keyup', $.proxy(this.findByName, this));
+	},
+	
+	initialize: function() {
+        var self = this;
+        this.store = new MemoryStore(function() {
+        	 self.showAlert('Store Initialized', 'Info');
+            self.renderHomeView();
+        });
     }
+   
+	    
+	 
+    
+    
 
 };
 
