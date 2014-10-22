bf401ffcbf6a9aef042abc618e3e06c3f9825b84
paulmoody <moodyweb@hotmail.com>
2014-10-22 11:47:16 -0430

    location api
    test the api

--- js/HomeView.js
+++ js/HomeView.js
@@ -1,4 +1,4 @@
-﻿var HomeView = function(store) {
+var HomeView = function(store) {
 
     this.initialize = function() {
         // Define a div wrapper for the view. The div wrapper is used to attach events.
@@ -12,17 +12,11 @@
     };
 
     this.findByName = function() {
-    store.findByName($('.search-key').val(), function(employees) {
-        $('.employee-list').html(HomeView.liTemplate(employees));
-        if (self.iscroll) {
-            console.log('Refresh iScroll');
-            self.iscroll.refresh();
-        } else {
-            console.log('New iScroll');
-            self.iscroll = new iScroll($('.scroll', self.el)[0], {hScrollbar: false, vScrollbar: false });
-        }
-    });
-};
+        store.findByName($('.search-key').val(), function(employees) {
+            $('.employee-list').html(HomeView.liTemplate(employees));
+        });
+    };
+
     this.initialize();
 
 }
--- js/main.js
+++ js/main.js
@@ -1,50 +1,58 @@
 var app = {
 
-	 showAlert: function (message, title) {
-	    if (navigator.notification) {
-	        navigator.notification.alert(message, null, title, 'OK');
-	    } else {
-	        alert(title ? (title + ": " + message) : message);
-	    }
-	},
+    showAlert: function (message, title) {
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
+    },
 
-    findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-           $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
+    registerEvents: function() {
+        $(window).on('hashchange', $.proxy(this.route, this));
+        $('body').on('mousedown', 'a', function(event) {
+            $(event.target).addClass('tappable-active');
+        });
+        $('body').on('mouseup', 'a', function(event) {
+            $(event.target).removeClass('tappable-active');
         });
     },
 
+    showAlert: function (message, title) {
+        if (navigator.notification) {
+            navigator.notification.alert(
+                message,
+                null, // callback
+                title,
+                'OK' // Button label
+            );
+        } else {
+            alert(title + ": " + message);
+        }
+    },
 
-	renderHomeView: function() {
-    var html =
-            "<div class='header'><h1>Home</h1></div>" +
-            "<div class='search-view'>" +
-            "<input class='search-key'/>" +
-            "<ul class='employee-list'></ul>" +
-	            "</div>"
-	    $('body').html(html);
-	    $('.search-key').on('keyup', $.proxy(this.findByName, this));
-	},
-	
-	initialize: function() {
+    route: function() {
+        var hash = window.location.hash;
+        if (!hash) {
+            $('body').html(new HomeView(this.store).render().el);
+            return;
+        }
+        var match = hash.match(app.detailsURL);
+        if (match) {
+            this.store.findById(Number(match[1]), function(employee) {
+                $('body').html(new EmployeeView(employee).render().el);
+            });
+        }
+    },
+
+    initialize: function() {
         var self = this;
+        this.detailsURL = /^#employees\/(\d{1,})/;
+        this.registerEvents();
         this.store = new MemoryStore(function() {
-        	 self.showAlert('Store Initialized', 'Info');
-            self.renderHomeView();
+            self.route();
         });
     }
-   
-	    
-	 
-    
-    
 
 };
 
